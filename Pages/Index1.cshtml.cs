baaa4b36303794d1a943d91d2145212b06b7b0bd
SK6739 <SK6739@WPC-SK673-2BG17>
2024-05-29 14:53:43 +0530

    Add project files.

---
+++ Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zebra_IT4IT_Reports.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
